1a5b10c5fb2692befd8dedd94c7fed4da40dff6b
Artyom Balashov <ten.que@yandex.ru>
2020-05-13 15:47:38 +0300

    Layout and some logic

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ Book.prototype.info = function () {
   const readOrNot = this.isRead ? 'read it!' : 'not read yet';
   return `${this.title} by ${this.author}, ${this.numPages} pages, ${readOrNot}`;
 }
+
+function addBookToLibrary() {
+  const title = prompt("Title: ");
+  const author = prompt("Author: ");
+  const pages = Number(prompt("Pages: "));
+  const isRead = Boolean(prompt("Have your read this book? true/false: "));
+  myLibrary.push(new Book(author, title, pages, isRead));
+}
+
+
+
+let myLibrary = [];
+const libContainer = document.querySelector('#lib-container');
+// addBookToLibrary();
+// console.table(myLibrary);
