ad81d50fbd588b8b0a42d4ce56fc1060fe77da8a
Artyom Balashov <ten.que@yandex.ru>
2020-05-13 17:17:03 +0300

    Refactor code a little bit

--- script.js
+++ script.js
@@ -6,20 +6,19 @@ function Book(author, title, numPages, isRead) {
 }
 
 Book.prototype.info = function () {
-  const readOrNot = this.isRead ? 'read it!' : 'not read yet';
-  return `${this.title} by ${this.author}, ${this.numPages} pages, ${readOrNot}`;
+  return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.isRead}`;
 }
 
 function addBookToLibrary() {
   const title = prompt("Title: ");
   const author = prompt("Author: ");
   const pages = Number(prompt("Pages: "));
-  const isRead = Boolean(prompt("Have your read this book? true/false: "));
+  const isRead = prompt("Have your read this book? Yes/No: ");
   myLibrary.push(new Book(author, title, pages, isRead));
 }
 
 function renderBook(book, idx) {
-  const tableRow = document.createElement('tr');
+  const tableRow = generateTableRowForBook(book);
   tableRow.setAttribute('data-value', `${idx}`);
 
   const removeButton = document.createElement('button');
@@ -27,21 +26,18 @@ function renderBook(book, idx) {
   removeButton.addEventListener("click", (e) => {
     removeBookFromLibrary(book, idx);
   });
-
-  const titleData = document.createElement('td');
-  const authorData = document.createElement('td');
-  const pagesData = document.createElement('td');
-  const readData = document.createElement('td');
-  titleData.textContent = book.title;
-  authorData.textContent = book.author;
-  pagesData.textContent = book.numPages;
-  readData.textContent = book.isRead ? "read!" : 'not read yet!';
-  tableRow.appendChild(titleData);
-  tableRow.appendChild(authorData);
-  tableRow.appendChild(pagesData);
-  tableRow.appendChild(readData);
   tableRow.appendChild(removeButton);
   libContainer.appendChild(tableRow);
+}
+
+function generateTableRowForBook(book) {
+  tr = document.createElement("tr");
+  for (let value of Object.values(book)) {
+    td = document.createElement('td');
+    td.textContent = value;
+    tr.appendChild(td);
+  }
+  return tr;
 }
 
 function render() {
@@ -59,29 +55,23 @@ function clearTable() {
   }
 }
 
-
 document.getElementById("add-book-button").addEventListener('click', (e) => {
   addBookToLibrary();
   render();
 });
 
 function removeBookFromLibrary(book, idx) {
-  console.log('Inside delete');
-  let i = myLibrary.indexOf(book);
-  console.log('Index: ' + idx);
   tableRow = document.querySelector(`tr[data-value="${idx}"]`);
   if (tableRow) {
-    console.log('to delete');
     let table = document.querySelector('#lib-container');
     table.removeChild(tableRow);
-    myLibrary.splice(i, 1);
+    myLibrary.splice(myLibrary.indexOf(book), 1);
   }
 }
 
 let myLibrary = [];
 const libContainer = document.querySelector('#lib-container');
-myLibrary.push(new Book('Haruki Murakami', 'Norwegian Wood', 500, true));
-myLibrary.push(new Book('J.K Rowling', 'Harry Potter', 600, true));
-myLibrary.push(new Book('Hendrik Dorgathen', 'Space dog', 50, false));
+myLibrary.push(new Book('Haruki Murakami', 'Norwegian Wood', 500, 'Yes'));
+myLibrary.push(new Book('J.K Rowling', 'Harry Potter', 600, 'No'));
+myLibrary.push(new Book('Hendrik Dorgathen', 'Space dog', 50, 'Yes'));
 render();
-// console.table(myLibrary);
